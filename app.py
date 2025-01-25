76e29607cebb0b38c4c6337efcb28f5fc13562c0
BoredToad <maxim.chapkanski@gmail.com>
2025-01-25 13:38:25 +0100

    basic student_page logic done :)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from auth import auth_bp, get_username_from_id
+from students import students, read_and_sort_csv
 
 app = Flask(__name__)
 app.config.from_mapping(SECRET_KEY='dev')
@@ -9,6 +10,7 @@ app.register_blueprint(auth_bp)
 @app.context_processor
 def processor():
     return dict(
+        read_and_sort_csv=read_and_sort_csv,
         get_username_from_id=get_username_from_id,
         int=int
     )
@@ -20,3 +22,5 @@ def index() -> str:
 @app.route("/rules")
 def rules():
     return render_template("homepage.jinja")
+
+app.route("/students")(students)
---
+++ students.py
@@ -0,0 +1,44 @@
+from collections.abc import Iterator
+import csv
+import datetime
+from flask import render_template
+from itertools import count
+
+def tail(it: Iterator) -> Iterator:
+    next(it)
+    return it
+
+def current_week() -> int:
+    return datetime.datetime.now().isocalendar().week
+
+def read_and_sort_csv() -> list[tuple[int, list[str]]]:
+    with open('static/students.csv') as file:
+        csv_reader = csv.DictReader(file, delimiter=",") # done so the list is not consumed
+        rows = list(csv_reader)
+        groups = []
+
+        for week in count(start=current_week()):
+            if str(week) not in csv_reader.fieldnames: # pyright: ignore
+                break
+
+            students = list(map(
+                lambda student: student["name"],
+                filter(
+                    lambda student: student[str(week)] == 'x',
+                    rows
+                )
+            ))
+
+            if not students: # filter out breaks and such
+                continue
+
+            groups.append((week, students))
+
+        return groups
+
+def students():
+    return render_template("students.jinja")
+
+
+if __name__ == "__main__":
+    read_and_sort_csv()
