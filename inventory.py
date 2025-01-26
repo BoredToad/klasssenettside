7d7d742b1e153201111090c185f022a82e943c4a
BoredToad <maxim.chapkanski@gmail.com>
2025-01-26 18:40:10 +0100

    basic filtering added, now I need search and the more complex edits that will involve some js

--- inventory.py
+++ inventory.py
@@ -1,12 +1,55 @@
+from collections.abc import ItemsView
 from flask import Blueprint, g, render_template, request
+from models import Item
+
+# note on how to interrupt table in future, the colspan tag is nice
+#<tr>
+#<th colspan="2" style="background-color:red;">
+#<h1>some interruption</h1>
+#</th>
+#</tr>
+availability_helper = lambda item: True if item == "available" else False
 
 inventory_bp = Blueprint("inventory", __name__, "/inventory")
 
+def get_and_filter_items(filters: dict[str, list[str]]) -> list[dict[str, str]]:
+    items = Item.select()
+
+    print(f"filters: \n{filters}")
+
+    if "availability" in filters:
+        items = items.where(
+            # I'm sad that we HAVE to convert it to a list for it to work
+            Item.availability.in_(list(map( # pyright: ignore
+                availability_helper,
+                filters["availability"]
+            )))
+        )
+    if "status" in filters:
+        items = items.where(
+            Item.status.in_(filters["status"]) # pyright: ignore
+        )
+    if "category" in filters:
+        items = items.where(
+            Item.category.in_(filters["category"]) # pyright: ignore
+        )
+
+    return [
+        {
+            "id": item, 
+            "name": item.name,
+            "availability": "available" if item.availability else "not available",
+            "status": item.status,
+            "manufacturer": item.manufacturer,
+            "model": item.model,
+            "category": item.category
+        }
+        for item in items
+    ]
+
 @inventory_bp.route("/inventory", methods=('GET', 'POST'))
 def inventory() -> str:
-    print("FORM----------------")
-    for (key, value) in request.form.items():
-        print(f"{key=}, values={request.form.getlist(key)}")
-
+    filters = { k : request.form.getlist(k) for k in request.form.keys() }
+    g.items = get_and_filter_items(filters)
 
     return render_template('inventory/inventory.jinja')
