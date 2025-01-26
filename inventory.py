1b48ed4dbf432327f5db21f7465e16dfe7969972
BoredToad <maxim.chapkanski@gmail.com>
2025-01-26 16:01:38 +0100

    inventory page form complete, not on to basic table

--- inventory.py
+++ inventory.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, g, render_template, request
 
 inventory_bp = Blueprint("inventory", __name__, "/inventory")
 
-@inventory_bp.route("/inventory")
+@inventory_bp.route("/inventory", methods=('GET', 'POST'))
 def inventory() -> str:
+    print("FORM----------------")
+    for (key, value) in request.form.items():
+        print(f"{key=}, values={request.form.getlist(key)}")
+
+
     return render_template('inventory/inventory.jinja')
